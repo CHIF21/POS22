22b592d7a79cf441131cbf379705ac4521bcd0d0
MrBrut <80398743+MrBrut-glitch@users.noreply.github.com>
2023-03-23 16:21:27 +0100

    Add files via upload

---
+++ Stoff/Demo_ListModels/src/demo_defaultlistmodel/Animal.java
@@ -0,0 +1,45 @@
+package demo_defaultlistmodel;
+
+import java.util.Objects;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String species;
+
+    public Animal(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s - %d", species, name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && Objects.equals(name, animal.name) && Objects.equals(species, animal.species);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, species);
+    }
+}
